327fe5798b2fa3e282e7f139cbc6a52360421b20
Kenji Nakamura <dragondev1017@gmail.com>
2021-07-09 09:41:23 +0300

    Update token contract

--- GreenToken.sol
+++ GreenToken.sol
@@ -26,10 +26,12 @@
 
  abstract contract Context {
     function _msgSender() internal view virtual returns (address payable) {
+
         return payable(msg.sender);
     }
 
     function _msgData() internal view virtual returns (bytes memory) {
+
         this; // silence state mutability warning without generating bytecode - see https://github.com/ethereum/solidity/issues/2691
         return msg.data;
     }
@@ -50,6 +52,7 @@ interface IERC20 {
 library SafeMath {
 
     function add(uint256 a, uint256 b) internal pure returns (uint256) {
+
         uint256 c = a + b;
         require(c >= a, "SafeMath: addition overflow");
 
@@ -57,10 +60,12 @@ library SafeMath {
     }
 
     function sub(uint256 a, uint256 b) internal pure returns (uint256) {
+
         return sub(a, b, "SafeMath: subtraction overflow");
     }
 
     function sub(uint256 a, uint256 b, string memory errorMessage) internal pure returns (uint256) {
+
         require(b <= a, errorMessage);
         uint256 c = a - b;
 
@@ -68,6 +73,7 @@ library SafeMath {
     }
 
     function mul(uint256 a, uint256 b) internal pure returns (uint256) {
+
         if (a == 0) {
             return 0;
         }
@@ -80,10 +86,12 @@ library SafeMath {
 
 
     function div(uint256 a, uint256 b) internal pure returns (uint256) {
+
         return div(a, b, "SafeMath: division by zero");
     }
 
     function div(uint256 a, uint256 b, string memory errorMessage) internal pure returns (uint256) {
+
         require(b > 0, errorMessage);
         uint256 c = a / b;
         // assert(a == b * c + a % b); // There is no case in which this doesn't hold
@@ -92,10 +100,12 @@ library SafeMath {
     }
 
     function mod(uint256 a, uint256 b) internal pure returns (uint256) {
+
         return mod(a, b, "SafeMath: modulo by zero");
     }
 
     function mod(uint256 a, uint256 b, string memory errorMessage) internal pure returns (uint256) {
+
         require(b != 0, errorMessage);
         return a % b;
     }
@@ -104,6 +114,7 @@ library SafeMath {
 library Address {
 
     function isContract(address account) internal view returns (bool) {
+
         // According to EIP-1052, 0x0 is the value returned for not-yet created accounts
         // and 0xc5d2460186f7233c927e7db2dcc703c0e500b653ca82273b7bfad8045d85a470 is returned
         // for accounts without code, i.e. `keccak256('')`
@@ -115,6 +126,7 @@ library Address {
     }
 
     function sendValue(address payable recipient, uint256 amount) internal {
+
         require(address(this).balance >= amount, "Address: insufficient balance");
 
         // solhint-disable-next-line avoid-low-level-calls, avoid-call-value
@@ -124,23 +136,28 @@ library Address {
 
 
     function functionCall(address target, bytes memory data) internal returns (bytes memory) {
+
       return functionCall(target, data, "Address: low-level call failed");
     }
 
     function functionCall(address target, bytes memory data, string memory errorMessage) internal returns (bytes memory) {
+
         return _functionCallWithValue(target, data, 0, errorMessage);
     }
 
     function functionCallWithValue(address target, bytes memory data, uint256 value) internal returns (bytes memory) {
+
         return functionCallWithValue(target, data, value, "Address: low-level call with value failed");
     }
 
     function functionCallWithValue(address target, bytes memory data, uint256 value, string memory errorMessage) internal returns (bytes memory) {
+
         require(address(this).balance >= value, "Address: insufficient balance for call");
         return _functionCallWithValue(target, data, value, errorMessage);
     }
 
     function _functionCallWithValue(address target, bytes memory data, uint256 weiValue, string memory errorMessage) private returns (bytes memory) {
+
         require(isContract(target), "Address: call to non-contract");
 
         (bool success, bytes memory returndata) = target.call{ value: weiValue }(data);
@@ -168,40 +185,48 @@ contract Ownable is Context {
     event OwnershipTransferred(address indexed previousOwner, address indexed newOwner);
 
     constructor () {
+
         address msgSender = _msgSender();
         _owner = msgSender;
         emit OwnershipTransferred(address(0), msgSender);
     }
 
     function owner() public view returns (address) {
+
         return _owner;
     }
 
     modifier onlyOwner() {
+
         require(_owner == _msgSender(), "Ownable: caller is not the owner");
         _;
     }
 
     function renounceOwnership() public virtual onlyOwner {
+
         emit OwnershipTransferred(_owner, address(0));
         _owner = address(0);
     }
 
-    function transferOwnership(address newOwner) public virtual onlyOwner {
+    function transferOwnership(address newOwner) public virtual onlyOwner() {
+
         require(newOwner != address(0), "Ownable: new owner is the zero address");
         emit OwnershipTransferred(_owner, newOwner);
         _owner = newOwner;
     }
 
     function getUnlockTime() public view returns (uint256) {
+
         return _lockTime;
     }
 
     function getTime() public view returns (uint256) {
+
         return block.timestamp;
     }
 
-    function lock(uint256 time) public virtual onlyOwner {
+    function lock(uint256 time) public virtual onlyOwner() {
+
         _previousOwner = _owner;
         _owner = address(0);
         _lockTime = block.timestamp + time;
@@ -209,6 +234,7 @@ contract Ownable is Context {
     }
 
     function unlock() public virtual {
+
         require(_previousOwner == msg.sender, "You don't have permission to unlock");
         require(block.timestamp > _lockTime , "Contract is locked until 7 days");
         emit OwnershipTransferred(_owner, _previousOwner);
@@ -432,7 +458,6 @@ contract DemoGreen is Context, IERC20, Ownable {
     mapping (address => mapping (address => uint256)) private _allowances;
 
     mapping (address => bool) private _isExcludedFromFee;
-
     mapping (address => bool) private _isExcluded;
     address[] private _excluded;
 
@@ -508,76 +533,92 @@ contract DemoGreen is Context, IERC20, Ownable {
     }
 
     function name() public view returns (string memory) {
+
         return _name;
     }
 
     function symbol() public view returns (string memory) {
+
         return _symbol;
     }
 
     function decimals() public view returns (uint8) {
+
         return _decimals;
     }
 
     function totalSupply() public view override returns (uint256) {
+
         return _tTotal;
     }
 
     function balanceOf(address account) public view override returns (uint256) {
+
         if (_isExcluded[account]) return _tOwned[account];
         return tokenFromReflection(_rOwned[account]);
     }
 
     function transfer(address recipient, uint256 amount) public override returns (bool) {
+
         _transfer(_msgSender(), recipient, amount);
         return true;
     }
 
     function allowance(address owner, address spender) public view override returns (uint256) {
+
         return _allowances[owner][spender];
     }
 
     function approve(address spender, uint256 amount) public override returns (bool) {
+
         _approve(_msgSender(), spender, amount);
         return true;
     }
 
     function transferFrom(address sender, address recipient, uint256 amount) public override returns (bool) {
+
         _transfer(sender, recipient, amount);
         _approve(sender, _msgSender(), _allowances[sender][_msgSender()].sub(amount, "ERC20: transfer amount exceeds allowance"));
         return true;
     }
 
     function increaseAllowance(address spender, uint256 addedValue) public virtual returns (bool) {
+
         _approve(_msgSender(), spender, _allowances[_msgSender()][spender].add(addedValue));
         return true;
     }
 
     function decreaseAllowance(address spender, uint256 subtractedValue) public virtual returns (bool) {
+
         _approve(_msgSender(), spender, _allowances[_msgSender()][spender].sub(subtractedValue, "ERC20: decreased allowance below zero"));
         return true;
     }
 
     function isExcludedFromReward(address account) public view returns (bool) {
+
         return _isExcluded[account];
     }
 
     function totalFees() public view returns (uint256) {
+
         return _tFeeTotal;
     }
 
     function minimumTokensBeforeSwapAmount() public view returns (uint256) {
+
         return minimumTokensBeforeSwap;
     }
 
     function buyBackUpperLimitAmount() public view returns (uint256) {
+
         return buyBackUpperLimit;
     }
 
     function deliver(uint256 tAmount) public {
+
         address sender = _msgSender();
         require(!_isExcluded[sender], "Excluded addresses cannot call this function");
-        (uint256 rAmount,,,,,) = _getValues(tAmount);
+        (uint256 rAmount, , , , , ) = _getValues(tAmount);
         _rOwned[sender] = _rOwned[sender].sub(rAmount);
         _rTotal = _rTotal.sub(rAmount);
         _tFeeTotal = _tFeeTotal.add(tAmount);
@@ -585,17 +626,19 @@ contract DemoGreen is Context, IERC20, Ownable {
 
 
     function reflectionFromToken(uint256 tAmount, bool deductTransferFee) public view returns(uint256) {
+
         require(tAmount <= _tTotal, "Amount must be less than supply");
         if (!deductTransferFee) {
-            (uint256 rAmount,,,,,) = _getValues(tAmount);
+            (uint256 rAmount, , , , , ) = _getValues(tAmount);
             return rAmount;
         } else {
-            (,uint256 rTransferAmount,,,,) = _getValues(tAmount);
+            (, uint256 rTransferAmount, , , , ) = _getValues(tAmount);
             return rTransferAmount;
         }
     }
 
     function tokenFromReflection(uint256 rAmount) public view returns(uint256) {
+
         require(rAmount <= _rTotal, "Amount must be less than total reflections");
         uint256 currentRate =  _getRate();
         return rAmount.div(currentRate);
@@ -612,7 +655,8 @@ contract DemoGreen is Context, IERC20, Ownable {
     }
 
     function includeInReward(address account) external onlyOwner() {
-        require(_isExcluded[account], "Account is already excluded");
+
+        require(_isExcluded[account], "Account is already included");
         for (uint256 i = 0; i < _excluded.length; i++) {
             if (_excluded[i] == account) {
                 _excluded[i] = _excluded[_excluded.length - 1];
@@ -625,6 +669,7 @@ contract DemoGreen is Context, IERC20, Ownable {
     }
 
     function _approve(address owner, address spender, uint256 amount) private {
+
         require(owner != address(0), "ERC20: approve from the zero address");
         require(spender != address(0), "ERC20: approve to the zero address");
 
@@ -637,6 +682,7 @@ contract DemoGreen is Context, IERC20, Ownable {
         address to,
         uint256 amount
     ) private {
+
         require(from != address(0), "ERC20: transfer from the zero address");
         require(to != address(0), "ERC20: transfer to the zero address");
         require(amount > 0, "Transfer amount must be greater than zero");
@@ -684,12 +730,14 @@ contract DemoGreen is Context, IERC20, Ownable {
 
 
     function buyBackTokens(uint256 amount) private lockTheSwap {
+
     	if (amount > 0) {
     	    swapETHForTokens(amount);
 	    }
     }
 
     function swapTokensForEth(uint256 tokenAmount) private {
+
         // generate the uniswap pair path of token -> weth
         address[] memory path = new address[](2);
         path[0] = address(this);
@@ -710,12 +758,13 @@ contract DemoGreen is Context, IERC20, Ownable {
     }
 
     function swapETHForTokens(uint256 amount) private {
+
         // generate the uniswap pair path of token -> weth
         address[] memory path = new address[](2);
         path[0] = uniswapV2Router.WETH();
         path[1] = address(this);
 
-      // make the swap
+        // make the swap
         uniswapV2Router.swapExactETHForTokensSupportingFeeOnTransferTokens{value: amount}(
             0, // accept any amount of Tokens
             path,
@@ -727,6 +776,7 @@ contract DemoGreen is Context, IERC20, Ownable {
     }
 
     function addLiquidity(uint256 tokenAmount, uint256 ethAmount) private {
+
         // approve token transfer to cover all possible scenarios
         _approve(address(this), address(uniswapV2Router), tokenAmount);
 
@@ -742,6 +792,7 @@ contract DemoGreen is Context, IERC20, Ownable {
     }
 
     function _tokenTransfer(address sender, address recipient, uint256 amount,bool takeFee) private {
+
         if(!takeFee)
             removeAllFee();
 
@@ -760,6 +811,7 @@ contract DemoGreen is Context, IERC20, Ownable {
     }
 
     function _transferStandard(address sender, address recipient, uint256 tAmount) private {
+
         (uint256 rAmount, uint256 rTransferAmount, uint256 rFee, uint256 tTransferAmount, uint256 tFee, uint256 tLiquidity) = _getValues(tAmount);
         _rOwned[sender] = _rOwned[sender].sub(rAmount);
         _rOwned[recipient] = _rOwned[recipient].add(rTransferAmount);
@@ -769,6 +821,7 @@ contract DemoGreen is Context, IERC20, Ownable {
     }
 
     function _transferToExcluded(address sender, address recipient, uint256 tAmount) private {
+
         (uint256 rAmount, uint256 rTransferAmount, uint256 rFee, uint256 tTransferAmount, uint256 tFee, uint256 tLiquidity) = _getValues(tAmount);
 	    _rOwned[sender] = _rOwned[sender].sub(rAmount);
         _tOwned[recipient] = _tOwned[recipient].add(tTransferAmount);
@@ -779,6 +832,7 @@ contract DemoGreen is Context, IERC20, Ownable {
     }
 
     function _transferFromExcluded(address sender, address recipient, uint256 tAmount) private {
+
         (uint256 rAmount, uint256 rTransferAmount, uint256 rFee, uint256 tTransferAmount, uint256 tFee, uint256 tLiquidity) = _getValues(tAmount);
     	_tOwned[sender] = _tOwned[sender].sub(tAmount);
         _rOwned[sender] = _rOwned[sender].sub(rAmount);
@@ -789,6 +843,7 @@ contract DemoGreen is Context, IERC20, Ownable {
     }
 
     function _transferBothExcluded(address sender, address recipient, uint256 tAmount) private {
+
         (uint256 rAmount, uint256 rTransferAmount, uint256 rFee, uint256 tTransferAmount, uint256 tFee, uint256 tLiquidity) = _getValues(tAmount);
     	_tOwned[sender] = _tOwned[sender].sub(tAmount);
         _rOwned[sender] = _rOwned[sender].sub(rAmount);
@@ -800,17 +855,20 @@ contract DemoGreen is Context, IERC20, Ownable {
     }
 
     function _reflectFee(uint256 rFee, uint256 tFee) private {
+
         _rTotal = _rTotal.sub(rFee);
         _tFeeTotal = _tFeeTotal.add(tFee);
     }
 
     function _getValues(uint256 tAmount) private view returns (uint256, uint256, uint256, uint256, uint256, uint256) {
+
         (uint256 tTransferAmount, uint256 tFee, uint256 tLiquidity) = _getTValues(tAmount);
         (uint256 rAmount, uint256 rTransferAmount, uint256 rFee) = _getRValues(tAmount, tFee, tLiquidity, _getRate());
         return (rAmount, rTransferAmount, rFee, tTransferAmount, tFee, tLiquidity);
     }
 
     function _getTValues(uint256 tAmount) private view returns (uint256, uint256, uint256) {
+
         uint256 tFee = calculateTaxFee(tAmount);
         uint256 tLiquidity = calculateLiquidityFee(tAmount);
         uint256 tTransferAmount = tAmount.sub(tFee).sub(tLiquidity);
@@ -818,6 +876,7 @@ contract DemoGreen is Context, IERC20, Ownable {
     }
 
     function _getRValues(uint256 tAmount, uint256 tFee, uint256 tLiquidity, uint256 currentRate) private pure returns (uint256, uint256, uint256) {
+
         uint256 rAmount = tAmount.mul(currentRate);
         uint256 rFee = tFee.mul(currentRate);
         uint256 rLiquidity = tLiquidity.mul(currentRate);
@@ -826,11 +885,13 @@ contract DemoGreen is Context, IERC20, Ownable {
     }
 
     function _getRate() private view returns(uint256) {
+
         (uint256 rSupply, uint256 tSupply) = _getCurrentSupply();
         return rSupply.div(tSupply);
     }
 
     function _getCurrentSupply() private view returns(uint256, uint256) {
+
         uint256 rSupply = _rTotal;
         uint256 tSupply = _tTotal;
         for (uint256 i = 0; i < _excluded.length; i++) {
@@ -843,6 +904,7 @@ contract DemoGreen is Context, IERC20, Ownable {
     }
 
     function _takeLiquidity(uint256 tLiquidity) private {
+
         uint256 currentRate =  _getRate();
         uint256 rLiquidity = tLiquidity.mul(currentRate);
         _rOwned[address(this)] = _rOwned[address(this)].add(rLiquidity);
@@ -851,18 +913,17 @@ contract DemoGreen is Context, IERC20, Ownable {
     }
 
     function calculateTaxFee(uint256 _amount) private view returns (uint256) {
-        return _amount.mul(_taxFee).div(
-            10**2
-        );
+
+        return _amount.mul(_taxFee).div(10**2);
     }
 
     function calculateLiquidityFee(uint256 _amount) private view returns (uint256) {
-        return _amount.mul(_liquidityFee).div(
-            10**2
-        );
+
+        return _amount.mul(_liquidityFee).div(10**2);
     }
 
     function removeAllFee() private {
+
         if(_taxFee == 0 && _liquidityFee == 0) return;
 
         _previousTaxFee = _taxFee;
@@ -873,68 +934,83 @@ contract DemoGreen is Context, IERC20, Ownable {
     }
 
     function restoreAllFee() private {
+
         _taxFee = _previousTaxFee;
         _liquidityFee = _previousLiquidityFee;
     }
 
     function isExcludedFromFee(address account) public view returns(bool) {
+
         return _isExcludedFromFee[account];
     }
 
-    function excludeFromFee(address account) public onlyOwner {
+    function excludeFromFee(address account) public onlyOwner() {
+
         _isExcludedFromFee[account] = true;
     }
 
-    function includeInFee(address account) public onlyOwner {
+    function includeInFee(address account) public onlyOwner() {
+
         _isExcludedFromFee[account] = false;
     }
 
     function setTaxFeePercent(uint256 taxFee) external onlyOwner() {
+
         _taxFee = taxFee;
     }
 
     function setLiquidityFeePercent(uint256 liquidityFee) external onlyOwner() {
+
         _liquidityFee = liquidityFee;
     }
 
     function setMaxTxAmount(uint256 maxTxAmount) external onlyOwner() {
+
         _maxTxAmount = maxTxAmount;
     }
 
     function setMarketingDivisor(uint256 divisor) external onlyOwner() {
+
         marketingDivisor = divisor;
     }
 
     function setNumTokensSellToAddToLiquidity(uint256 _minimumTokensBeforeSwap) external onlyOwner() {
+
         minimumTokensBeforeSwap = _minimumTokensBeforeSwap;
     }
 
      function setBuybackUpperLimit(uint256 buyBackLimit) external onlyOwner() {
+
         buyBackUpperLimit = buyBackLimit * 10**18;
     }
 
     function setMarketingAddress(address _rewardAddress) external onlyOwner() {
+
         rewardAddress = payable(_rewardAddress);
     }
 
-    function setSwapAndLiquifyEnabled(bool _enabled) public onlyOwner {
+    function setSwapAndLiquifyEnabled(bool _enabled) public onlyOwner() {
+
         swapAndLiquifyEnabled = _enabled;
         emit SwapAndLiquifyEnabledUpdated(_enabled);
     }
 
-    function setBuyBackEnabled(bool _enabled) public onlyOwner {
+    function setBuyBackEnabled(bool _enabled) public onlyOwner() {
+
         buyBackEnabled = _enabled;
         emit BuyBackEnabledUpdated(_enabled);
     }
 
-    function prepareForPreSale() external onlyOwner {
+    function prepareForPreSale() external onlyOwner() {
+
         setSwapAndLiquifyEnabled(false);
         _taxFee = 0;
         _liquidityFee = 0;
         _maxTxAmount = 1 * 10**15 * 10**9;
     }
 
-    function afterPreSale() external onlyOwner {
+    function afterPreSale() external onlyOwner() {
+
         setSwapAndLiquifyEnabled(true);
         _taxFee = 5;
         _liquidityFee = 10;
@@ -942,6 +1018,7 @@ contract DemoGreen is Context, IERC20, Ownable {
     }
 
     function transferToAddressETH(address payable recipient, uint256 amount) private {
+
         recipient.transfer(amount);
     }
 
