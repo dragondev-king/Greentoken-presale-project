e423c996e144b99f909011b49bce41e9a3b0593c
dragondev-king <dragondev1017@gmail.com>
2021-09-29 07:04:34 +0300

    added node modules

--- truffle-config.js
+++ truffle-config.js
@@ -18,6 +18,7 @@
  *
  */
 
+require("dotenv").config();
 const HDWalletProvider = require("@truffle/hdwallet-provider");
 
 module.exports = {
@@ -32,36 +33,18 @@ module.exports = {
     // websocket: true        // Enable EventEmitter interface for web3 (default: false)
     // },
     // Useful for deploying to a public network.
-    ropsten: {
+    kovan: {
       provider: () =>
         new HDWalletProvider(
-          mnemonic,
-          `https://ropsten.infura.io/v3/YOUR-PROJECT-ID`
+          process.env.MNEMONIC,
+          `https://kovan.infura.io/v3/${process.env.PROJECT_ID}`
         ),
-      network_id: 3, // Ropsten's id
+      network_id: 42, // Kovan's id
       gas: 5500000, // Ropsten has a lower block limit than mainnet
       confirmations: 2, // # of confs to wait between deployments. (default: 0)
       timeoutBlocks: 200, // # of blocks before a deployment times out  (minimum/default: 50)
       skipDryRun: true, // Skip dry run before migrations? (default: false for public nets )
     },
-    ropsten: {
-      provider: () =>
-        new HDWalletProvider(
-          mnemonic,
-          `https://ropsten.infura.io/v3/YOUR-PROJECT-ID`
-        ),
-      network_id: 3, // Ropsten's id
-      gas: 5500000, // Ropsten has a lower block limit than mainnet
-      confirmations: 2, // # of confs to wait between deployments. (default: 0)
-      timeoutBlocks: 200, // # of blocks before a deployment times out  (minimum/default: 50)
-      skipDryRun: true, // Skip dry run before migrations? (default: false for public nets )
-    },
-    // Useful for private networks
-    // private: {
-    // provider: () => new HDWalletProvider(mnemonic, `https://network.io`),
-    // network_id: 2111,   // This network is yours, in the cloud.
-    // production: true    // Treats this network as if it was a public net. (default: false)
-    // }
   },
 
   // Set default mocha options here, use special reporters etc.
