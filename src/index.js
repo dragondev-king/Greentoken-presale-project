2e6e8024b0321f03f20ed354c35e6573550a52ae
Kenji Nakamura <dragondev1017@gmail.com>
2021-10-01 15:42:04 +0300

    made presale migration.

---
+++ src/index.js
@@ -0,0 +1,12 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "containers/app";
+import "styles/styles.css";
+
+console.log(123);
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById("root")
+);
