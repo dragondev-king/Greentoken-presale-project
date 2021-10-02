24ed5908eb4b0b3e9aa91c768eefb4ced27cc345
Kenji Nakamura <dragondev1017@gmail.com>
2021-10-02 04:42:52 +0300

    made simple ui

--- src/App.js
+++ src/App.js
@@ -1,36 +1,20 @@
-import "./App.css";
+import "./App.scss";
 
 import { useState, useEffect } from "react";
 import { ethers } from "ethers";
 import axios from "axios";
 
-import { mintNFT } from "./helpers/interact";
-import {
-  getTokenIdsOfWallet,
-  getCurrentTotalSupply,
-  getCurrentMaxSupply,
-  getCurrentMaxMint,
-  getOccupiedIds,
-} from "./helpers/contract";
+import { getContractWithSigner } from "./helpers/contract";
 import { connectWallet, getCurrentWalletConnected } from "./helpers/wallet";
 
 const App = () => {
   const [walletAddress, setWalletAddress] = useState("");
-  const [soldOutCounts, setSoldOutCounts] = useState(0);
-  const [maxSupply, setMaxSupply] = useState(0);
-  const [maxMint, setMaxMint] = useState(0);
-  const [initialIds, setInitialIds] = useState([]);
-
-  const [status, setStatus] = useState("");
-  const [error, setError] = useState("");
-
-  const [newMint, setNewMint] = useState([]);
-  const [mintCount, setMintCount] = useState(1);
-  const [mintLoading, setMintLoading] = useState(false);
-
-  const [metadatas, setMetadatas] = useState([]);
-
-  const [collapseExpanded, setCollapseExpanded] = useState(false);
+  const [walletStatus, setWalletStatus] = useState("");
+  const [addresses, setAddresses] = useState([]);
+  const [attenders, setAttenders] = useState([]);
+  const [address, setAddress] = useState("");
+  const [ratio, setRatio] = useState("");
+  const [airDrop, setAirDrop] = useState();
 
   useEffect(() => {
     const initDatas = async () => {
@@ -38,82 +22,23 @@ const App = () => {
         const { address, status } = await getCurrentWalletConnected();
 
         setWalletAddress(address);
-        setStatus(status);
+        setWalletStatus(status);
 
         onChangeWalletListener();
         onConnectWalletHandler();
 
-        let totalSupply = await getCurrentTotalSupply();
-        setSoldOutCounts(totalSupply);
-
-        let maxSupply = await getCurrentMaxSupply();
-        setMaxSupply(maxSupply);
-
-        let maxMint = await getCurrentMaxMint();
-        setMaxMint(maxMint);
-
-        const initIds = generateInitIds();
-        setInitialIds(initIds);
+        const value = getContractWithSigner();
+        setAirDrop(value);
       }
     };
 
     initDatas();
-
-    window.addEventListener("resize", getWindowWidth);
-    return () => window.removeEventListener("resize", getWindowWidth);
   }, []);
-
-  // Fetch CA Ids of the Wallet
-  useEffect(() => {
-    const getIdsFromWallet = async () => {
-      if (!!walletAddress) {
-        let tokenIdsOfWallet = await getTokenIdsOfWallet(walletAddress);
-
-        fetchMetaDatas(tokenIdsOfWallet);
-      }
-    };
-
-    getIdsFromWallet();
-  }, [walletAddress]);
-
-  useEffect(() => {
-    const getSoldOuts = async () => {
-      if (window.ethereum) {
-        let totalSupply = await getCurrentTotalSupply();
-
-        setSoldOutCounts(totalSupply);
-
-        if (newMint[0] && newMint[0].toLowerCase() === walletAddress) {
-          let tokenIdsOfWallet = await getTokenIdsOfWallet(walletAddress);
-
-          fetchMetaDatas(tokenIdsOfWallet);
-        }
-      }
-    };
-
-    getSoldOuts();
-  }, [newMint]);
-
-  // When the width of the website exceeds 1024px, hide sidebar
-  const getWindowWidth = () => {
-    const { innerWidth: width } = window;
-    if (width > 1024) {
-      setCollapseExpanded(false);
-    }
-  };
-
-  const onCollapseExpandHandler = () => {
-    setCollapseExpanded(!collapseExpanded);
-  };
-
-  const onAlertClickHandler = () => {
-    setError("");
-  };
 
   const onConnectWalletHandler = async () => {
     const walletResponse = await connectWallet();
 
-    setStatus(walletResponse.status);
+    setWalletStatus(walletResponse.status);
     setWalletAddress(walletResponse.address);
   };
 
@@ -122,10 +47,10 @@ const App = () => {
       window.ethereum.on("accountsChanged", (accounts) => {
         if (accounts.length) {
           setWalletAddress(accounts[0]);
-          setStatus("Get your CryptoAthletes pack, 0.05ETH");
+          setWalletStatus("Connected");
         } else {
           setWalletAddress("");
-          setStatus("Connect Metamask");
+          setWalletStatus("Connect Metamask");
         }
       });
 
@@ -133,7 +58,7 @@ const App = () => {
         onConnectWalletHandler();
       });
     } else {
-      setStatus(
+      setWalletStatus(
         <p>
           🦊 You must install Metamask, a virtual Ethereum wallet, in your
           browser.(https://metamask.io/download.html)
@@ -142,90 +67,72 @@ const App = () => {
     }
   };
 
-  const fetchMetaDatas = async (ids) => {
-    let metadatas = [];
+  const onChangeHandler = (e) => {
+    e.preventDefault();
+    const { value, name } = e.target;
 
-    for (let i = 0; i < ids.length; i++) {
-      await axios
-        .get(
-          `https://gateway.pinata.cloud/ipfs/Qmd3cb8TsvPWFJuyyKD3KYjn7Sh7Ht8q3CUvsZtPz7n9Qw/CAhooper${ids[i]}.json`
-        )
-        .then((response) => {
-          metadatas.push(response.data);
-        })
-        .catch((err) => {
-          console.log(err);
-        });
-    }
-
-    setMetadatas(metadatas);
-  };
-
-  const onMintCountChangeHandler = (e) => {
-    let value =
-      e.target.value > maxMint
-        ? maxMint
-        : e.target.value < 1
-        ? 1
-        : e.target.value;
-
-    setMintCount(value);
-  };
-
-  const generateInitIds = () => {
-    let initIds = [];
-
-    for (let i = 0; i < 10020; i++) {
-      initIds.push(i + 1);
-    }
-
-    return initIds;
-  };
-
-  const getDiffArray = (source, target) => {
-    return source.filter((index) => {
-      let tempArray = [];
-      for (let i = 0; i < target.length; i++) {
-        tempArray.push(ethers.BigNumber.from(target[i]).toNumber());
-      }
-
-      return tempArray.indexOf(index) < 0;
-    });
-  };
-
-  const getRandomIds = async () => {
-    let customIds = [];
-    const occupied = await getOccupiedIds();
-    const diffIds = getDiffArray(initialIds, occupied);
-
-    while (customIds.length < mintCount) {
-      const id = Math.floor(Math.random() * diffIds.length);
-      const index = diffIds[id];
-      customIds.push(index);
-    }
-
-    return customIds;
-  };
-
-  const onMintHandler = async () => {
-    if (!!walletAddress) {
-      setMintLoading(true);
-      const randomIds = await getRandomIds();
-
-      const { _, status } = await mintNFT(
-        walletAddress,
-        setMintLoading,
-        setNewMint,
-        randomIds
-      );
-
-      if (status.indexOf("insufficient fund") >= 0) {
-        setError("You don't have enough eths to mint CA!");
-      }
+    switch (name) {
+      case "address":
+        setAddress(value);
+        break;
+      case "ratio":
+        setRatio(value);
+        break;
+      default:
+        break;
     }
   };
 
-  return <div>123123s</div>;
+  const onAddHandler = () => {
+    setAttenders([
+      ...attenders,
+      {
+        address,
+        ratio,
+      },
+    ]);
+  };
+
+  const generateTable = () =>
+    attenders.length
+      ? attenders.map((data, index) => (
+          <div key={index} className="app-attenders-item">
+            <span>{data.address}</span>:<span>{data.ratio}</span>
+          </div>
+        ))
+      : "";
+
+  const startContest = async () => {
+    var addressArr = Object.entries(attenders).map(([key, val]) => val.address);
+    console.log("sec airdrop123", airDrop);
+    const result = await airDrop.initContest(addressArr);
+    console.log("con", result);
+  };
+
+  const claimBNBs = async () => {
+    const result = await airDrop.claimBNBs();
+    console.log("claim", result);
+  };
+
+  const stopAirDrop = async () => {
+    const result = await airDrop.stopAirDrop();
+    console.log("stop", result);
+  };
+  return (
+    <div className="app flex flex-column">
+      <div className="app-attenders">{generateTable()}</div>
+      <div className="app-inputs">
+        <input name="address" type="text" onChange={onChangeHandler} />
+        <input name="ratio" type="text" onChange={onChangeHandler} />
+        <button onClick={onAddHandler}>Add</button>
+      </div>
+      <div className="app-controllers">
+        <button onClick={startContest}>Start Contest</button>
+        <button onClick={stopAirDrop}>Stop Contest</button>
+        <button onClick={claimBNBs}>Claim</button>
+      </div>
+    </div>
+  );
 };
 
 export default App;
--- src/helpers/contract.js
+++ src/helpers/contract.js
@@ -6,10 +6,10 @@ require("dotenv").config();
 // Contract can be used to write Contract
 export const getContractWithSigner = () => {
   const infuraProvider = new ethers.providers.Web3Provider(window.ethereum);
-  const signer = infuraProvider.getSigner();
 
+  const signer = infuraProvider.getSigner();
   const contract = new ethers.Contract(
-    process.env.CONTRACT_ADDRESS,
+    process.env.REACT_APP_AIRDROP_ADDRESS,
     contractABI.abi,
     signer
   );
@@ -22,7 +22,7 @@ const getContractWithoutSigner = () => {
   const infuraProvider = new ethers.providers.Web3Provider(window.ethereum);
 
   const contract = new ethers.Contract(
-    process.env.CONTRACT_ADDRESS,
+    process.env.REACT_APP_AIRDROP_ADDRESS,
     contractABI.abi,
     infuraProvider
   );
