da9f521ee03f754cffcdbd5ecb9b2723cd403af0
dragondev-king <dragondev1017@gmail.com>
2021-10-02 22:57:07 +0300

    Update project

--- migrations/4_deploy_airdrop.js
+++ migrations/4_deploy_airdrop.js
@@ -2,9 +2,5 @@ require("dotenv").config();
 const AirDrop = artifacts.require("AirDrop");
 
 module.exports = function (deployer) {
-  deployer.deploy(
-    AirDrop,
-    `${process.env.PRESALE_WALLET_ADDRESS}`,
-    parseInt(process.env.END_AIRDROP_AT)
-  );
+  deployer.deploy(AirDrop, `${process.env.PRESALE_WALLET_ADDRESS}`);
 };
